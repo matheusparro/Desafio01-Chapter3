0def087fe0364520988398316498e9b32b486dad
matheusparro <mathparro@gmail.com>
2021-04-02 00:46:31 -0300

    teste

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -6,13 +6,18 @@ import Prismic from '@prismicio/client';
 import { useRouter } from 'next/router';
 import { format, parseISO } from 'date-fns';
 import { ptBR, enUS } from 'date-fns/locale';
+import Head from 'next/head';
+import Link from 'next/link';
 import Header from '../../components/Header';
 import { getPrismicClient } from '../../services/prismic';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { Comments } from '../../components/Comments/index';
 
 interface Post {
   first_publication_date: string | null;
+  last_publication_date: string | null;
+  uid: string;
   data: {
     title: string;
     banner: {
@@ -36,6 +41,7 @@ interface PostProps {
 
 export default function Post({
   post,
+
   nextPost,
   prevPost,
   preview,
@@ -66,18 +72,26 @@ export default function Post({
 
   return (
     <>
+      <Head>
+        <title>Post | spacetraveling</title>
+      </Head>
       <Header />
+
       <div className={styles.imagePost}>
         <img src={post.data.banner.url} alt="imagem" width="100%" />
       </div>
       <article className={styles.container}>
         <a>
           <strong>{post.data.title}</strong>
-          <div>
+
+          <div className={styles.infoHeader}>
             <span>
               <FiCalendar color="#BBBBBB" />
 
-              {format(parseISO(post.first_publication_date), 'dd MMM yyyy', {
+              {/* {format(parseISO(post.first_publication_date), 'dd MMM yyyy', {
+                locale: ptBR,
+              })} */}
+              {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
                 locale: ptBR,
               })}
             </span>
@@ -92,6 +106,15 @@ export default function Post({
               {timeToRead} min
             </span>
           </div>
+          {post.first_publication_date !== post.last_publication_date && (
+            <a className={styles.aada}>{`* editado em ${format(
+              new Date(post.first_publication_date),
+              'dd MMM yyyy',
+              {
+                locale: ptBR,
+              }
+            )}`}</a>
+          )}
         </a>
         {post.data.content.map(postData => (
           <div key={postData.heading} className={styles.postSection}>
@@ -103,17 +126,38 @@ export default function Post({
             />
           </div>
         ))}
+        {preview && (
+          <aside className={styles.exitPreview}>
+            <Link href="/api/exit-preview">
+              <a>Sair do modo Preview</a>
+            </Link>
+          </aside>
+        )}
+        <div className={styles.navigateTool}>
+          {prevPost && (
+            <Link href={`/post/${prevPost.uid}`}>
+              <a className={styles.prevPost}>
+                {prevPost.data.title}
+                <span>Post Anterior</span>
+              </a>
+            </Link>
+          )}
+          {nextPost && (
+            <Link href={`/post/${nextPost.uid}`}>
+              <a className={styles.nextPost}>
+                {nextPost?.data.title}
+                <span>Próximo Post</span>
+              </a>
+            </Link>
+          )}
+        </div>
       </article>
+      <div className={styles.Comments}>
+        <Comments />
+      </div>
     </>
   );
 }
-
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
-
-//   // TODO
-// };
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient();
@@ -131,12 +175,43 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: true,
   };
 };
-export const getStaticProps: GetServerSideProps = async context => {
-  const { slug } = context.params;
+export const getStaticProps: GetServerSideProps = async ({
+  params: { slug },
+  preview = false,
+  previewData,
+}) => {
   const prismic = getPrismicClient();
-  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const response = await prismic.getByUID('posts', String(slug), {
+    ref: previewData?.ref ?? null,
+  });
+
+  if (!response) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const prevPost = (
+    await prismic.query(Prismic.predicates.at('document.type', 'posts'), {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date desc]',
+      fetch: ['posts.title'],
+    })
+  ).results[0];
+  const nextPost = (
+    await prismic.query(Prismic.predicates.at('document.type', 'posts'), {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+      fetch: ['posts.title'],
+    })
+  ).results[0];
+  console.log(JSON.stringify(nextPost, null, 2));
   const post = {
     first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
     uid: response.uid,
     data: {
       banner: {
@@ -150,6 +225,11 @@ export const getStaticProps: GetServerSideProps = async context => {
   };
 
   return {
-    props: { post },
+    props: {
+      post,
+      preview,
+      nextPost: nextPost ?? null,
+      prevPost: prevPost ?? null,
+    },
   };
 };
